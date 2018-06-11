514f8284920c6c75645fe7b0675118b9dc172bbd
pbrudny <pbrudny@gmail.com>
2018-06-11 13:08:23 +0200

    Fix random number contract

--- 03_basic_random.sol
+++ 03_basic_random.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.23;
 
 contract BasicRandom {
     uint randNonce = 0;
 
     function getRandomNumber() public returns(uint) {
-        uint rand = uint(keccak256(now, msg.sender, randNonce));
+        uint rand = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce)));
         randNonce++;        
         return rand;
     }
