c08b585389051c65bf4815e9a1c854410f0f74df
pbrudny <pbrudny@gmail.com>
2018-07-09 11:20:42 +0200

    improve basic token

--- 08_basic_token/08_basic_token.sol
+++ 08_basic_token/08_basic_token.sol
@@ -1,18 +1,24 @@
 pragma solidity ^0.4.22;
 
 contract BasicToken {
-    mapping(address=>uint) public balances;
+    uint public initialSupply;
+
+    mapping(address=>uint) balances;
     
     constructor(uint _initialSupply) public {
+        initialSupply = _initialSupply;
         balances[msg.sender] = _initialSupply;
     }
     
     function transfer(address _recipient, uint _amount) public {
         require(balances[msg.sender] >= _amount, "Not enough funds");
         require(_recipient != msg.sender, "No need to send tokens to yourself");
-        //overflow check
-        require(balances[_recipient] + _amount > balances[_recipient]);
+        require(balances[_recipient] + _amount > balances[_recipient]); //overflow check
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
+    }
+
+    function balanceOf(address _owner) public view returns (uint) {
+        return balances[_owner];
     }
 }
