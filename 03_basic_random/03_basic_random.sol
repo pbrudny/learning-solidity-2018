187863b5d4efce03db06109a5d9a6b8eac046186
Murat Can <141832088+codcreater1@users.noreply.github.com>
2024-06-12 01:32:56 +0300

    Update 03_basic_random.sol

--- 03_basic_random/03_basic_random.sol
+++ 03_basic_random/03_basic_random.sol
@@ -1,11 +1,12 @@
-pragma solidity ^0.4.23;
+
+pragma solidity ^0.8.0;
 
 contract BasicRandom {
-    uint randNonce = 0;
+    uint private randNonce = 0;
 
-    function getRandomNumber() public returns(uint) {
-        uint rand = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce)));
-        randNonce++;        
+    function getRandomNumber() public returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce)));
+        randNonce++;
         return rand;
     }
 }
