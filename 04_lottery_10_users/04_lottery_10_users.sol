1d1247417c5de5bc86f6b52c05769d712ec2c8a6
Murat Can <141832088+codcreater1@users.noreply.github.com>
2024-06-12 14:16:16 +0300

    Update 04_lottery_10_users.sol

--- 04_lottery_10_users/04_lottery_10_users.sol
+++ 04_lottery_10_users/04_lottery_10_users.sol
@@ -1,44 +1,63 @@
-pragma solidity ^0.4.23;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Lottery10Users {
-    address[10] participants;
-    uint8 participantsCount = 0;
-    uint randNonce = 0;
+    address[10] public participants;
+    uint8 public participantsCount;
+    uint private randNonce;
+    uint public ticketPrice = 0.1 ether;
+    //just added ticketPrice for this line also added public and private methods
 
-    function join() public payable {
-        require(msg.value == 0.1 ether, "Must send 0.1 ether");
+    event LotteryWinner(address indexed winner, uint amount);
+    //Events: Added an event LotteryWinner to log the winner and the prize amount.
+
+    constructor() {
+        participantsCount = 0;
+        randNonce = 0;
+    }
+
+    function join() external payable { // changeced it with external
+        require(msg.value == ticketPrice, "Must send 0.1 ether");
         require(participantsCount < 10, "User limit reached");
-        require(joinedAlready(msg.sender) == false, "User already joined");
+        require(!joinedAlready(msg.sender), "User already joined");
+
         participants[participantsCount] = msg.sender;
         participantsCount++;
+
         if (participantsCount == 10) {
             selectWinner();
         }
-    }
+    } // added if block here for selectWinner
     
-    function joinedAlready(address _participant) private view returns(bool) {
-        bool containsParticipant = false;
-        for(uint i = 0; i < 10; i++) {
+    function joinedAlready(address _participant) private view returns (bool) {
+        for (uint i = 0; i < participantsCount; i++) {
             if (participants[i] == _participant) {
-                containsParticipant = true;
+                return true;
             }
         }
-        return containsParticipant;
+        return false;
     }
     
-    function selectWinner() private returns(address) {
+    function selectWinner() private {
         require(participantsCount == 10, "Waiting for more users");
+
         address winner = participants[randomNumber()];
-        winner.transfer(address(this).balance);
-        delete participants;
+        uint prize = address(this).balance;
+        (bool success, ) = winner.call{value: prize}("");
+        require(success, "Transfer failed");
+
+        emit LotteryWinner(winner, prize);
+
+        // Reset lottery for the next round
+        for (uint i = 0; i < 10; i++) {
+            participants[i] = address(0);
+        }
         participantsCount = 0;
-        return winner;
     }
     
-    function randomNumber() private returns(uint) {
-        uint rand = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % 10;
+    function randomNumber() private returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % 10;
         randNonce++;
         return rand;
     }
-        
 }
